4d36c4da198eb864a4adb187c4b16a26be7212a6
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2018-10-25 13:45:06 -0300

    Add files via upload

--- BancoDeDadosProjeto.sql
+++ BancoDeDadosProjeto.sql
@@ -2,18 +2,37 @@ create table Usuario(
 codUsuario int identity(1,1) primary key,
 nome varchar(50) not null,
 CPF char(14) not null,
-telefone varchar(15) not null,
+telefone varchar(20) not null,
 email varchar(50) not null,
 senha varchar(30) not null
 )
 
 select * from Usuario
+drop table Acesso
 
 create table Acesso(
-idUsuario varchar(30) primary key,
-senha varchar(30) not null,
-dataAcesso datetime not null
+email varchar(50) primary key,
+senha varchar(30) not null
 )
+
+create trigger UsuarioAcesso_tg on Usuario 
+instead of insert
+as 
+declare @nome varchar(50)
+declare @cpf char(14)
+declare @telefone varchar(20)
+declare @email varchar(50)
+declare @senha varchar(30)
+
+select @nome=nome from inserted
+select @cpf=cpf from inserted
+select @telefone=telefone from inserted
+select @email=email from inserted
+select @senha=senha from inserted
+if not exists(select * from Acesso where email = @email)
+	insert into Usuario values (@nome, @cpf, @telefone, @email, @senha)
+else 
+	print 'Não foi possível adicionar'
 
 create table Gasto(
 codGasto int primary key,

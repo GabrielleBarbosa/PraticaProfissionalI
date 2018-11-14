e2fa4b6a346f402056d22d4e3376eced612cbef9
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2018-11-14 10:41:43 -0200

    Add files via upload

--- Site/js/Gastos.js
+++ Site/js/Gastos.js
@@ -1,5 +1,4 @@
 var tipoAtual = "";
-var btnAlterar;
 
 /////////////////////////////////////
 listarGastos = function (tipo) {
@@ -27,6 +26,9 @@ listarGastos = function (tipo) {
                                         title: "Alterar gasto",
                                         src: "Imagens/edit.png" });
             btnAlterar.append(icone);
+            btnAlterar.click(function(){
+                abrirModal(val.nome, val.valor);
+            });
             // fim do botao alterar
 
             // --- criar botao EXCLUIR que vai ser um link
@@ -47,13 +49,13 @@ listarGastos = function (tipo) {
 
             tdBotoes.append(btnAlterar);
             tdBotoes.append(btnExcluir);
-
+            
             //colunas da tabela
             if(val.nome == "Total")
             {
                 tr.append($("<td style='color: red;' />").text(val.nome));
                 if (val.valor == null)
-                    tr.append($("<td />").text("R$ 0,00"));
+                    tr.append($("<td />").text("R$ 0.00"));
                 else
                     tr.append($("<td />").text("R$ " + val.valor));
                 tr.append("<td></td>");
@@ -61,9 +63,7 @@ listarGastos = function (tipo) {
             else
             {
                 tr.append($("<td />").text(val.nome));
-                if (val.valor == null)
-                    tr.append($("<td />").text("R$ 0,00"));
-                else
+                if (val.valor != null)
                     tr.append($("<td />").text("R$ " + val.valor));
                 
                 tr.append(tdBotoes);
@@ -80,16 +80,17 @@ listarGastos = function (tipo) {
 
 /////////////////////////////////////
 excluir = function(nome){ 
-        if(confirm("Tem certeza que deseja excluir?")){
-                $.ajax({
-                    url: "http://localhost:3000/Gasto/"+nome+"/"+tipoAtual,
-                    type: 'DELETE'
-                }).done(function(){
-                    //chamar listarItem
-                    listarGastos(tipoAtual);
+    if(confirm("Tem certeza que deseja excluir?")){
+            $.ajax({
+                url: "http://localhost:3000/Gasto/"+nome+"/"+tipoAtual,
+                type: 'DELETE'
+            }).done(function(){
+                //chamar listarItem
+                listarGastos(tipoAtual);
 
-                }); //done
-        }
+            }); //done
+    }
+    listarGastos(tipoAtual);
 };
     
 /////////////////////////////////////
@@ -97,18 +98,35 @@ incluir = function(form, tipo){
     $.post( "http://localhost:3000/Gasto/" + tipo, form.serialize() ).done(function(data){
         if (!data.erro) {
             form.each(function(data){
-                    //limpar formulário
                     this.reset();
             });
-
-            $("#modalInc").closeModal();
+            
+        }
+        alert(data.mensagem);
+    });
+    form.each(function(){
+            this.reset();
+    });
+    $("#modalAlt").closeModal();
+    listarGastos(tipoAtual);
+};   
+///////////////////////////////////
+alterar = function(form, nome){
+     $.post( "http://localhost:3000/Gasto/" + nome + "/" + tipoAtual, form.serialize() ).done(function(data){
+        if (!data.erro) {
+            form.each(function(){
+                    this.reset();
+            });
         }
         alert(data.mensagem);
 
-        //chamar listarItem
-        listarGastos(tipoAtual);
     });
-};   
+    form.each(function(){
+            this.reset();
+    });
+    $("#modalAlt").closeModal();
+    listarGastos(tipoAtual);
+};
 
 /////////////////////////////////////////MODAL INCLUSAO//////////////////////////////////////////////////////
 // Get the modal
@@ -150,7 +168,7 @@ window.onclick = function(event) {
 
 // When the user clicks on the button, open the modal 
 btnCadastrar.onclick = function() {
-    var regExValor = /^[0-9]*,[0-9]{2}$/;
+    var regExValor = /^[0-9]*\.[0-9]{2}$/;
     var validaValor = regExValor.test($("#valor").val());
     
     var tipo = $("input[name='tipo']:checked").val();
@@ -160,7 +178,7 @@ btnCadastrar.onclick = function() {
     if(validaValor && $("#nome").val() != "")
         incluir($("#formInclusao"), tipo);
     else
-        alert("Não foi possível incluir, verifique os dados! \nOBS: o valor deve ter duas casas decimais");
+        alert("Não foi possível incluir, verifique os dados! \nOBS: o valor deve ter duas casas decimais(99.99)");
 };
 
 /////////////////////////////////////////MODAL ALTERAÇÃO//////////////////////////////////////////////////////
@@ -168,48 +186,51 @@ btnCadastrar.onclick = function() {
 var modal2 = document.getElementById('modalAlt');
 
 // Get the <span> element that closes the modal
-var span2 = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
 
 // Get the link CANCELAR
 var btnCancelar2 = document.getElementById("btnCancelar2");
 
 var btnAlteracao = document.getElementById("btnAlteracao");
 
+var nomeA = "";
+
 // When the user clicks on the button, open the modal 
-btnAlterar.onclick = function() {
+abrirModal = function(n, v) {
+    nomeA = n;
+    $("#nomeAlt").text("Gasto: " + n);
+    $("#tipoAlt").text("Tipo: " + tipoAtual);
+    $("#valorAlt").text("ValorAntigo: " + v);
     modal2.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
 span2.onclick = function() {
     modal2.style.display = "none";
+    listarGastos(tipoAtual);
 };
 
 // When the user clicks on <span> (x), close the modal
-btnCancelar.onclick = function() {
+btnCancelar2.onclick = function() {
     modal2.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modal2) {
         modal2.style.display = "none";
     }
 };
 
 // When the user clicks on the button, open the modal 
 btnAlteracao.onclick = function() {
-    var regExValor = /^[0-9]*,[0-9]{2}$/;
-    var validaValor = regExValor.test($("#valor").val());
+    var regExValor = /^[0-9]*\.[0-9]{2}$/;
+    var validaValor = regExValor.test($("#valorNovo").val());
     
-    var par = $(this).parent().parent();
-
-    alert(tipo);
-    
-    if(validaValor && $("#nome").val() != "")
-        incluir($("#formInclusao"), tipo);
+    if(validaValor)
+        alterar($("#formAlteracao"), nomeA);
     else
-        alert("Não foi possível incluir, verifique os dados! \nOBS: o valor deve ter duas casas decimais");
+        alert("Não foi possível alterar, verifique os dados! \nOBS: o valor deve ter duas casas decimais(99.99)");
 };
 
 
